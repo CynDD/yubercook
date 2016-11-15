1c1e38bd5cc9da21da97b826622f4153e036cb1d
Emanuel Chalela <emanuelchalela@gmail.com>
2016-11-15 02:04:52 -0300

    conexion hosting

--- conexion.php
+++ conexion.php
@@ -1,5 +1,28 @@
 <?php
 
+//Variables for connecting to your database.
+//These variable values come from your hosting account.
+$hostname = "pepuru.db.10593088.hostedresource.com";
+$username = "pepuru";
+$dbname = "pepuru";
+
+//These variable values need to be changed by you before deploying
+$password = "WWtotWdUN1!";
+$usertable = "ing3";
+$yourfield = "test";
+
+//Connecting to your database
+mysql_connect($hostname, $username, $password) OR DIE ("Unable to
+connect to database! Please try again later.");
+mysql_select_db($dbname);
+
+//Fetching from your database table.
+$query = "SELECT * FROM $usertable";
+$result = mysql_query($query);
+
+
+/*
+
         // CONECTAR AL SERVIDOR DE BASE DE DATOS
     $conex = @mysql_connect('127.0.0.1','root','');
 
@@ -16,5 +39,5 @@
     if (!$selDB) {
         die("ATENCION!!!.. NO se pudo SELECCIONAR Base de Datos");
     } // endif
-
+*/
 ?>
